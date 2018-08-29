26ad0f1f91f1ae501b75c0f92b12dbb80d8e2917
Predrag <idmpepe@gmail.com>
2018-08-29 14:09:38 +0200

    Resotre cursor positon when the views reopen

--- core/ViewsManager.py
+++ core/ViewsManager.py
@@ -1,10 +1,15 @@
+import sublime
+
+
 class ViewsManager:
     ''' Responsible for storing views and reopening them later. '''
 
-    # {'window_id' : [ views ]}
+    # {'window_id' : [ views.file_names ]}
     previous_views = {}
-    # { 'window_id' : view }
+    # {'window_id' : view.file_name }
     last_active_view = {}
+    # {'window_id': pos}
+    last_cursor_pos = {}
 
     is_open = False
 
@@ -25,12 +30,24 @@ class ViewsManager:
 
         last_active_view = self._get_last_active_view()
         if last_active_view:
-            self.window.open_file(last_active_view)
+            view = self.window.open_file(last_active_view)
+
+            def trying_restoring_the_cursor(view):
+                if not view.is_loading():
+                    self._restore_cursor_pos(view)
+                else:
+                    # cant put the cursor if the view is not loaded
+                    # so try it again
+                    sublime.set_timeout(lambda:
+                                        trying_restoring_the_cursor(view), 20)
+
+            trying_restoring_the_cursor(view)
         self._clear_state()
 
     def save_views_for_later(self):
-        last_active_view = self.window.active_view().file_name()
-        self._save_last_active_view(last_active_view)
+        view = self.window.active_view()
+        self._save_last_active_view(view)
+        self._save_last_cursor_pos(view)
         self._save_views(self.window.views())
 
     def get_views(self):
@@ -39,8 +56,22 @@ class ViewsManager:
     def _get_last_active_view(self):
         return self.last_active_view.get(self.window.id())
 
-    def _save_last_active_view(self, file_name):
-        self.last_active_view[self.window.id()] = file_name
+    def _restore_cursor_pos(self, view):
+        cursor_pos = self.last_cursor_pos.get(self.window.id())
+
+        # put the cursor there
+        sel = view.sel()
+        sel.clear()
+        sel.add(cursor_pos)
+
+        # show the position at center
+        view.show_at_center(cursor_pos)
+
+    def _save_last_active_view(self, view):
+        self.last_active_view[self.window.id()] = view.file_name()
+
+    def _save_last_cursor_pos(self, view):
+        self.last_cursor_pos[self.window.id()] = view.sel()[0].begin()
 
     def _save_views(self, views):
         self.previous_views[self.window.id()] = []
