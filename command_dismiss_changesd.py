1c80f6c456fdfb1b5b3c98de809805564b1410d6
Предраг Николић <idmpepe@gmail.com>
2023-10-05 09:37:55 +0200

    Update command_dismiss_changesd.py

--- command_dismiss_changesd.py
+++ command_dismiss_changesd.py
@@ -1,9 +1,72 @@
+import os
+from .core.status_view import get_status_view
 from .core.git_commands import Git
 from .core.git_diff_view import GitDiffView
 from .utils import get_line
 import sublime
 import sublime_plugin
 
+# command: git_diff_view_dismiss_hunk_changes
+class GitDiffViewDismissHunkChangesCommand(sublime_plugin.TextCommand):
+    def run(self, _: sublime.Edit) -> None:
+        window = self.view.window()
+        if not window:
+            return
+        diff_view = self.view
+        sel = diff_view.sel()
+        if not sel:
+            return
+        cursor = sel[0].b
+        status_view = get_status_view(window.views())
+        if not status_view:
+            return
+        line = get_line(status_view)
+        if line is None:
+            return
+        git = Git(window)
+        git_statuses = GitDiffView.git_statuses[window.id()]
+        git_status = git_statuses[line]
+        if not git_status:
+            return
+        head = git.diff_head(git_status['file_name'])
+
+        start_patch = diff_view.find('^@@', cursor, sublime.REVERSE)
+        if not start_patch:
+            return
+
+        end_patch = diff_view.find('^@@', cursor)
+        if not end_patch:
+            end_patch = sublime.Region(diff_view.size())
+        hunk_content = diff_view.substr(sublime.Region(start_patch.begin(), end_patch.begin()))
+        patch_content = f"{head}\n{hunk_content}"
+
+        not_staged = not git_status['is_staged'] or diff_view.find('^Unstaged', cursor, sublime.REVERSE)
+        temp_patch_file = os.path.join(sublime.cache_path(), 'temp_patch_file.patch')
+        patch_file = open(temp_patch_file, 'w', encoding='utf-8')
+        try:
+            patch_file.write(patch_content)
+            patch_file.close()
+            if not_staged:
+                git.discard_patch(temp_patch_file)
+                print('run')
+        finally:
+            patch_file.close()
+            os.remove(patch_file.name)
+
+        new_git_statuses = git.git_statuses()
+        status_view.run_command('update_status_view', {
+            'git_statuses': new_git_statuses,
+        })
+        try:
+            git_status = new_git_statuses[line]
+            self.view.run_command("update_diff_view", {
+                'git_status': git_status,
+            })
+        except:
+            print('ovde')
+            self.view.run_command("update_diff_view", {
+                'git_status': None,
+            })
 
 # command: git_diff_view_dismiss_changes
 class GitDiffViewDismissChangesCommand(sublime_plugin.TextCommand):
@@ -34,9 +97,15 @@ class GitDiffViewDismissChangesCommand(sublime_plugin.TextCommand):
             self.view.run_command('update_status_view', {
                 'git_statuses': git_statuses,
             })
-            self.view.run_command("update_diff_view", {
-                'git_status': git_status,
-            })
+            try:
+                git_status = git_statuses[line]
+                self.view.run_command("update_diff_view", {
+                    'git_status': git_status,
+                })
+            except:
+                self.view.run_command("update_diff_view", {
+                    'git_status': None,
+                })
 
         self.view.show_popup_menu([
             'Confirm Discard'
